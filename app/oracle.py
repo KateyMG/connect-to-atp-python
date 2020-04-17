042438e2a55cbcac23eea9f9836ebea850ae9b7e
Katherine Mazariegos <katherinemazariegos@ufm.edu>
2020-04-17 10:26:55 -0600

    Flask routes

--- app/oracle.py
+++ app/oracle.py
@@ -15,14 +15,31 @@ print(rs.fetchall())
 print('hola')
 
 #Conteo de registros de todas las tablas de HR
+@app.route('/Conteo_Registros')
+def count():
+    rs= cursor.execute("select sum(to_number(extractvalue(xmltype(dbms_xmlgen.getxml(\'select count(*) c from \'||table_name)),\'/ROWSET/ROW/C\'))) count from user_tables")
+    return print(rs.fetchall())
+
 
 #Insertar EMPLEADO, PUESTO, REGION, PAIS, LOCATION 
+@app.route('/Insertar')
+def insertar():
+    return 'Insertar'
 
 #Actualizar puesto, salario de EMPLEADO (por id)
+@app.route('/Actualizar')
+def actualizar():
+    return 'Actualizar'
 
 #Eliminar EMPLEADO (por id)
+@app.route('/Eliminar')
+def eliminar():
+    return 'Eliminar'
 
 #Consultar Empleado (por id)
+@app.route('/Consultar')
+def consultar():
+    return 'Consultar'
 
 
 @app.route('/')
