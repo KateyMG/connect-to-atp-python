93aa6d7639266f725287789a68f7bbfeb292626e
Katherine Mazariegos <katherinemazariegos@ufm.edu>
2020-04-17 09:23:28 -0600

    Add flask to Dockerfile and python file

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,9 @@ RUN yum update -y && \
   yum -y install python-pip && \
   pip install -U pip && \
   python -m pip install cx_Oracle && \
+  python -m pip install flask &&\
   yum install -y oracle-instantclient19.3-basic.x86_64 && \
   yum clean all && \
   echo Installed
+EXPOSE 3000
 CMD ["python", "oracle.py"]
--- app/oracle.py
+++ app/oracle.py
@@ -1,6 +1,8 @@
 import cx_Oracle
 import os
-
+import flask
+from flask import Flask, jsonify, request
+app = Flask(__name__)
 connection = cx_Oracle.connect(os.environ['DB_USER'], os.environ['DB_PASSWORD'], os.environ['DB_CONNECTIONSTRING'])
 
 cursor = connection.cursor()
@@ -11,3 +13,21 @@ print(rs.fetchall())
 rs = cursor.execute("select * from employees")
 print(rs.fetchall())
 print('hola')
+
+#Conteo de registros de todas las tablas de HR
+
+#Insertar EMPLEADO, PUESTO, REGION, PAIS, LOCATION 
+
+#Actualizar puesto, salario de EMPLEADO (por id)
+
+#Eliminar EMPLEADO (por id)
+
+#Consultar Empleado (por id)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
